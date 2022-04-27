10b7e3a43a0883bd85afa806a9613a7e86a94b91
sasshu <winter12shu27@gmail.com>
2022-04-28 02:26:25 +0900

    create interface

--- App.jsx
+++ App.jsx
@@ -1,57 +1,65 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Button,
+} from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 const App = () => {
-  // シール台紙State
-  const [seal, setSeal] = useState([]);
+  // シール添付済台紙State
+  const [seals, setSeals] = useState([]);
   // シール点数State
   const [point, setPoint] = useState(0);
+  // シール最大添付枚数
+  const maxarea = Array(30);
+  maxarea.fill(0);
+  const [noseals, setNoseals] = useState([...maxarea]);
+
+  const onPressSeal = () => {
+    // 添付済シール情報をコピー
+    const newSeals = [...seals];
+    // 新たなポイント情報をシール台紙に追加
+    newSeals.push(noseals.length);
+    setSeals(newSeals);
+    // ポイント入力完了
+    setPoint(0);
+    // 未添付のシール情報をコピー
+    const newNoseals = [...noseals];
+    // シール添付可能な容量を削減
+    newNoseals.pop();
+    setNoseals(newNoseals);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.top}>
-        <Text style={styles.note}>目標：点</Text>
+        <Text style={styles.note}>あと点</Text>
+        <View style={styles.setting}>
+          <Icon name="settings" size={40} onPress={onPressSeal} />
+        </View>
       </View>
       <View style={styles.board}>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
-        <View style={styles.seal}></View>
+        {seals.map((seal, index) => (
+          <View key={index} style={styles.seal}>
+            <Text style={styles.sealtext}>{seal}</Text>
+          </View>
+        ))}
+        {noseals.map((noseal, index) => (
+          <View key={noseals.length + index} style={styles.noseal}></View>
+        ))}
       </View>
-      <View style={styles.bottom}>
-        <Text style={styles.note}>あと点（残り日）</Text>
-      </View>
+      <TouchableHighlight
+        onPress={onPressSeal}
+        underlayColor={"white"}
+        activeOpacity={"0.6"}
+      >
+        <View style={styles.sealbutton}>
+          <Text style={styles.buttontext}>シールを貼る</Text>
+        </View>
+      </TouchableHighlight>
     </View>
   );
 };
@@ -63,35 +71,61 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   board: {
-    height: "80%",
     width: "100%",
-    margin: "1%",
-    /*
-    borderWidth: 1,
-    borderRadius: 30,
-    borderColor: "red",
-    */
+    marginTop: "5%",
     flexDirection: "row",
     flexWrap: "wrap",
   },
   top: {
-    paddingTop: "7%",
-    height: "10%",
+    height: "12%",
+    width: "100%",
+    paddingTop: "4%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "pink",
   },
-  bottom: {
-    height: "9%",
+  setting: {
+    position: "absolute",
+    right: 15,
+    top: 28,
   },
   note: {
     fontSize: 30,
+    fontWeight: "bold",
+  },
+  noseal: {
+    height: 70,
+    width: 70,
+    margin: "0.5%",
+    borderStyle: "dotted",
+    borderRadius: 50,
+    borderWidth: 3,
+    borderColor: "pink",
   },
   seal: {
     height: 70,
     width: 70,
     margin: "0.5%",
-    justifyContent: "space-between",
-    borderStyle: "dotted",
     borderRadius: 50,
-    borderWidth: 3,
-    borderColor: "pink",
+    backgroundColor: "#ff006f",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sealbutton: {
+    marginTop: "8%",
+    backgroundColor: "#ff2599",
+    borderRadius: 20,
+    paddingHorizontal: 50,
+    paddingVertical: 25,
+  },
+  buttontext: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "white",
+  },
+  sealtext: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "white",
   },
 });
