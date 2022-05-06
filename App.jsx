9b5b16c1a4e1d174c675e2535e3a75ec43bbf586
sasshu <winter12shu27@gmail.com>
2022-05-07 01:20:18 +0900

    locate seals anywhere

--- App.jsx
+++ App.jsx
@@ -7,62 +7,108 @@ import {
   Modal,
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
+import Swiper from "react-native-swiper";
 import Icon from "react-native-vector-icons/MaterialIcons";
 
 const App = () => {
-  // シール添付済台紙State
-  const [seals, setSeals] = useState([]);
-  // シール点数State
-  const [point, setPoint] = useState(0.5);
   // シール最大添付枚数
   const maxarea = Array(30);
   maxarea.fill(0);
-  const [noSeals, setNoSeals] = useState([...maxarea]);
-  // 目標シール点数
-  const [goalPoint, setGoalPoint] = useState(28);
+  // シール台紙
+  const [collects, setCollects] = useState(maxarea);
+  // シール最大点数
+  const maxPoint = 3;
+  // シール点数
+  const [point, setPoint] = useState(0.5);
+  // 点数は0.5からmaxPointまで0.5刻み
+  const selectPoint = [];
+  for (let index = 0.5; index <= maxPoint; index += 0.5) {
+    selectPoint.push(index);
+  }
+  // 皿の獲得目標枚数
+  const [goalPlate, setGoalPlate] = useState(1);
+  // 枚数は1から10まで
+  const selectPlate = [];
+  for (let index = 0; index < 10; index++) {
+    selectPlate.push(index + 1);
+  }
+  // 28点で皿が1枚もらえる
+  const platePoint = 28;
   // 残りシール点数
-  const [needPoint, setNeedPoint] = useState(goalPoint);
+  const [needPoint, setNeedPoint] = useState(platePoint * goalPlate);
   // シール点数入力画面
   const [addModalVisible, setAddModalVisible] = useState(false);
   // シール削除画面
   const [deleteModalVisible, setDeleteModalVisible] = useState(false);
   // シール位置
   const [position, setPosition] = useState(0);
+  // 設定画面
+  const [configModalVisible, setConfigModalVisible] = useState(false);
+  // その他
 
-  const addSeal = () => {
-    // 添付済シール情報をコピー
-    const newSeals = [...seals];
-    // 新たなポイント情報をシール台紙に追加
-    newSeals.push(point);
-    setSeals(newSeals);
-    // 残りポイントを更新
-    setNeedPoint(needPoint - point);
-    // 未添付のシール情報をコピー
-    const newNoSeals = [...noSeals];
-    // シール添付可能な容量を削減
-    newNoSeals.pop();
-    setNoSeals(newNoSeals);
-    // 入力画面を閉じる
-    setAddModalVisible(false);
+  // Picker作成
+  // 引数：数値配列
+  const setPicker = (array) => {
+    return array.map((element, index) => (
+      <Picker.Item key={this} label={`${element}`} value={element} />
+    ));
   };
 
+  // 目標点更新
+  const updateSetting = () => {
+    let collectedPoint = 0;
+    collects.map((collect, index) => {
+      collectedPoint += collect;
+    });
+    setNeedPoint(platePoint * goalPlate - collectedPoint);
+    // 設定画面を閉じる
+    setConfigModalVisible(() => !configModalVisible);
+  };
+
+  // シールを追加
+  const addSeal = () => {
+    // シール情報をコピー
+    const newSeals = [...collects];
+    // 新たなポイント情報をシール台紙に追加
+    newSeals[position] = point;
+    setCollects(newSeals);
+    // 残りポイントを更新
+    setNeedPoint(() => needPoint - point);
+    // 入力画面を閉じる
+    setAddModalVisible(() => !addModalVisible);
+  };
+
+  // シールを削除
   const deleteSeal = () => {
-    // 添付済シール情報をコピー
-    const newSeals = [...seals];
+    // シール情報をコピー
+    const newSeals = [...collects];
     // 該当シールのポイントを取得
     const delPoint = newSeals[position];
     // 残りポイントを更新
     setNeedPoint(needPoint + delPoint);
     // ポイント情報を削除
-    newSeals.splice(position, 1);
-    setSeals(newSeals);
-    // 未添付のシール情報をコピー
-    const newNoSeals = [...noSeals];
-    // シール添付可能な容量を追加
-    newNoSeals.push(0);
-    setNoSeals(newNoSeals);
+    newSeals[position] = 0;
+    setCollects(newSeals);
     // 削除画面を閉じる
-    setDeleteModalVisible(false);
+    setDeleteModalVisible(() => !deleteModalVisible);
+  };
+
+  // シール台紙を初期化
+  const initializSeal = () => {
+    // シール情報をコピー
+    const newSeals = [...collects];
+    // シールをすべて削除
+    newSeals.fill(0);
+    setCollects(newSeals);
+    // 残りポイントを更新
+    setNeedPoint(() => platePoint * goalPlate);
+  };
+
+  // シールが貼られていない最初の位置を返す
+  const noSealPlace = () => {
+    for (let index = 0; index < maxarea.length; index++) {
+      if (collects[index] == 0) return index;
+    }
   };
 
   return (
@@ -70,30 +116,42 @@ const App = () => {
       <View style={styles.top}>
         <Text style={styles.note}>あと{needPoint}点</Text>
         <View style={styles.setting}>
-          <Icon name="settings" size={40} />
+          <Icon
+            name="settings"
+            size={40}
+            onPress={() => setConfigModalVisible(() => !configModalVisible)}
+          />
         </View>
       </View>
+
       <View style={styles.board}>
-        {seals.map((seal, index) => (
+        {collects.map((collect, index) => (
           <TouchableHighlight
             key={index}
-            style={styles.seal}
+            style={collect > 0 ? styles.seal : styles.noSeal}
             onPress={() => {
               setPosition(index);
-              setDeleteModalVisible(true);
+              collect > 0
+                ? setDeleteModalVisible(() => !deleteModalVisible)
+                : setAddModalVisible(() => !addModalVisible);
             }}
             underlayColor="pink"
             activeOpacity={0.6}
           >
-            <Text style={styles.sealText}>{seal}</Text>
+            {collect > 0 ? (
+              <Text style={styles.sealText}>{collect}</Text>
+            ) : (
+              <></>
+            )}
           </TouchableHighlight>
         ))}
-        {noSeals.map((noSeal, index) => (
-          <View key={seals.length + index} style={styles.noSeal}></View>
-        ))}
       </View>
+
       <TouchableHighlight
-        onPress={() => setAddModalVisible(true)}
+        onPress={() => {
+          setPosition(noSealPlace);
+          setAddModalVisible(() => !addModalVisible);
+        }}
         underlayColor="white"
         activeOpacity={0.6}
       >
@@ -109,7 +167,7 @@ const App = () => {
               <Icon
                 name="close"
                 size={40}
-                onPress={() => setAddModalVisible(false)}
+                onPress={() => setAddModalVisible(() => !addModalVisible)}
               />
             </View>
             <Picker
@@ -118,12 +176,7 @@ const App = () => {
               style={styles.picker}
               itemStyle={styles.pickerItem}
             >
-              <Picker.Item label="0.5" value={0.5} />
-              <Picker.Item label="1" value={1} />
-              <Picker.Item label="1.5" value={1.5} />
-              <Picker.Item label="2" value={2} />
-              <Picker.Item label="2.5" value={2.5} />
-              <Picker.Item label="3" value={3} />
+              {setPicker(selectPoint)}
             </Picker>
             <TouchableHighlight
               onPress={addSeal}
@@ -145,12 +198,11 @@ const App = () => {
         <View style={styles.modalCenter}>
           <View style={styles.editSeal}>
             <Text style={styles.note}>
-              {seals[position]}点シールを{"\n"}1枚はがしますか？
+              {collects[position]}点シールを{"\n"}1枚はがしますか？
             </Text>
-
             <View style={styles.yesnoButton}>
               <TouchableHighlight
-                onPress={() => setDeleteModalVisible(false)}
+                onPress={() => setDeleteModalVisible(() => !deleteModalVisible)}
                 underlayColor="#ddd"
                 activeOpacity={0.6}
               >
@@ -168,6 +220,41 @@ const App = () => {
                 </View>
               </TouchableHighlight>
             </View>
+          </View>
+        </View>
+      </Modal>
+      <Modal
+        visible={configModalVisible}
+        transparent={true}
+        animationType="slide"
+      >
+        <View style={styles.modalSide}>
+          <View style={styles.configBoard}>
+            <View style={styles.setting}>
+              <Icon
+                name="close"
+                size={40}
+                onPress={() => setConfigModalVisible(() => !configModalVisible)}
+              />
+            </View>
+            <Text style={styles.note}>目標のお皿の枚数</Text>
+            <Picker
+              selectedValue={goalPlate}
+              onValueChange={(itemValue) => setGoalPlate(itemValue)}
+              style={styles.picker}
+              itemStyle={styles.pickerItem}
+            >
+              {setPicker(selectPlate)}
+            </Picker>
+            <TouchableHighlight
+              onPress={updateSetting}
+              underlayColor="#ddd"
+              activeOpacity={0.6}
+            >
+              <View style={styles.confirmButton}>
+                <Text style={styles.buttonText}>変更</Text>
+              </View>
+            </TouchableHighlight>
           </View>
         </View>
       </Modal>
@@ -294,5 +381,16 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     paddingVertical: 20,
     marginHorizontal: 10,
+  },
+  modalSide: {
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+  configBoard: {
+    alignItems: "center",
+    width: 300,
+    height: "100%",
+    paddingTop: 100,
+    backgroundColor: "#ddd",
   },
 });
